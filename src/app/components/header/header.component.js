b92589bd114a79cebd2520004f0dfa588049ace9
Andy Williamson <email@andywilliamson.io>
2020-07-03 18:24:48 +0100

    Added fix to mobile nav and ignore file

--- src/app/components/header/header.component.js
+++ src/app/components/header/header.component.js
@@ -5,10 +5,10 @@ import './header.component.css';
 const Header = props => {
     return (
         <nav className="c-nav c-nav--fixed">
-            <a href="#" className="c-logo">BASE</a>
+            <a href="#" class="c-logo">BASE</a>
             <a href="http://www.codedeveloper.co.uk" className="c-nav--link">CodeDeveloper Website</a>
-            <a href="/" className="c-nav--link" className={props.page === 'home' ? 'is-active' : ''}><i class=" material-icons">home</i> Home</a>
-            <a href="/add" className="c-nav--link" className={props.page === 'add' ? 'is-active' : ''}><i class=" material-icons">add</i> Add</a>
+            <a href="/" className={props.page === 'home' ? 'c-nav--link is-active' : 'c-nav--link'}><i class=" material-icons">home</i> Home</a>
+            <a href="/add" className={props.page === 'add' ? 'c-nav--link is-active' : 'c-nav--link'}><i class=" material-icons">add</i> Add</a>
         </nav>
     );
 };
