4c840e21471ab2e320ea6526cd766e5b343505fd
Andy Williamson <email@andywilliamson.io>
2020-07-03 16:09:52 +0100

    Added routing

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 import Header from "./components/header/header.component";
-import find from './functions/global.functions'
+// import find from './functions/global.functions'
 
 import './app.css';
 
@@ -18,23 +23,53 @@ class App extends Component {
 
     render() {
         return (
-            <div className='App'>
-                <Header
-                    item={this.state.item}
-                />
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
 
-                <div className="c-container">
-                    <section className="c-page">
-                        <h1>BASE - A React Framework Seed</h1>
-
-                        <p>This site contains code snippets to help setup your app more quickly.</p>
-
-                        <p>This site also uses STYL by CodeDeveloper, a super lightweight UI theme only 82kb</p>
-                    </section>
-                </div>
-            </div>
+                    <Route exact path="/add">
+                        <Add />
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
+}
+
+function Home() {
+    return (
+        <div className='App'>
+            <Header page="home" />
+
+            <div className="c-container">
+                <section className="c-page">
+                    <h1>BASE - A React Framework Seed</h1>
+
+                    <p>This site contains code snippets to help setup your app more quickly.</p>
+
+                    <p>This site also uses STYL by CodeDeveloper, a super lightweight UI theme only 82kb</p>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+function Add() {
+    return (
+        <div className='App'>
+            <Header page="add" />
+
+            <div className="c-container">
+                <section className="c-page">
+                    <h1>Add</h1>
+
+                    <p>This site also uses STYL by CodeDeveloper, a super lightweight UI theme only 82kb</p>
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/app/components/header/header.component.js
+++ src/app/components/header/header.component.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import './header.component.css';
 
@@ -7,9 +7,10 @@ const Header = props => {
         <nav className="c-nav c-nav--fixed">
             <a href="#" className="c-logo">BASE</a>
             <a href="http://www.codedeveloper.co.uk" className="c-nav--link">CodeDeveloper Website</a>
-            <a href="javascript:void(0)" className="c-nav--link"><i class=" material-icons">home</i> Home</a>
+            <a href="/" className="c-nav--link" className={props.page === 'home' ? 'is-active' : ''}><i class=" material-icons">home</i> Home</a>
+            <a href="/add" className="c-nav--link" className={props.page === 'add' ? 'is-active' : ''}><i class=" material-icons">add</i> Add</a>
         </nav>
     );
-}
+};
 
 export default Header;
